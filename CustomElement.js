b20d535b9edc7c8ec5f7dee73896aff5b8e917d4
Mike Maley <michael.r.maley@gmail.com>
2025-05-06 19:31:21 -0400

    update CustomElement.js

---
+++ CustomElement.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+    function updateMathMLOutput(aElement) {
+      var tex = aElement.textContent,
+        display = aElement.getAttribute("display"),
+        dir = aElement.getAttribute("dir");
+      try {
+        // Parse the LaTeX input and replace it with the MathML output.
+        aElement.shadowRoot.innerHTML = TeXZilla.toMathMLString(
+          tex,
+          display === "block",
+          dir === "rtl",
+          true
+        );
+      } catch (e) {
+        // Parsing failed: use an <merror> with the original LaTeX input.
+        aElement.shadowRoot.innerHTML =
+          "<math><merror><mtext>" + tex + "</mtext></merror></math>";
+      }
+      aElement.shadowRoot.style = "math{font-family: inherit;}";
+    }
+
+    class LaTeX_Element extends HTMLElement {
+        constructor() {
+          super()
+          this.attachShadow({ mode: "open" });
+          this.mo = new MutationObserver((recs) => {
+            updateMathMLOutput(this);
+          })
+          this.mo.observe(this, { characterData: true, childList: true, attributes: true });
+        }
+
+        attributeChangedCallback(aName, aOld, aNew) {
+          if (aName === "dir" || aName === "display") {
+            if (aNew === null) {
+              this.shadowRoot.firstElementChild.removeAttribute(aName);
+            } else {
+              this.shadowRoot.firstElementChild.setAttribute(aName, aNew);
+            }
+          }
+        }
+    } 
+
+    customElements.define("la-tex", LaTeX_Element);
+  })();
