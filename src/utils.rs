885484b5e047e0a7298cc3101f7ca1f230dbc0b5
Mike Maley <michael.r.maley@gmail.com>
2025-08-14 19:26:32 -0400

    update src/utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -8,3 +8,11 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+
+#[allow(unused)]
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
