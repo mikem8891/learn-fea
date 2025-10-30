569d73d2c1ba1663744db1784ee928a0b779459e
Mike Maley <michael.r.maley@gmail.com>
2025-10-30 19:29:08 -0400

    latex-element to module

--- src/latex-element.js
+++ src/latex-element.js
@@ -1,49 +1,58 @@
-(function() {
-    "use strict";
+// @ts-check
 
-    const SHEET = new CSSStyleSheet();
-    SHEET.replaceSync("math{font-family: inherit; overflow-x: auto; overflow-y: hidden;}");
-    
-    function updateMathMLOutput(aElement) {
-      var tex = aElement.textContent,
-        display = aElement.getAttribute("display"),
-        dir = aElement.getAttribute("dir");
-      try {
-        // Parse the LaTeX input and replace it with the MathML output.
-        aElement.shadowRoot.innerHTML = TeXZilla.toMathMLString(
-          tex,
-          display === "block",
-          dir === "rtl",
-          true
-        );
-      } catch (e) {
-        // Parsing failed: use an <merror> with the original LaTeX input.
-        aElement.shadowRoot.innerHTML =
-          "<math><merror><mtext>" + tex + "</mtext></merror></math>";
-      }
-      aElement.shadowRoot.adoptedStyleSheets = [SHEET];
+const SHEET = new CSSStyleSheet();
+SHEET.replaceSync("math{font-family: inherit; overflow-x: auto; overflow-y: hidden;}");
+
+/**
+ * @param {LatexElement} aElement
+ */
+function updateMathMLOutput(aElement) {
+  var tex = aElement.textContent,
+    display = aElement.getAttribute("display"),
+    dir = aElement.getAttribute("dir");
+  const shadowRoot = /** @type {ShadowRoot} */(aElement.shadowRoot);
+  try {
+    // Parse the LaTeX input and replace it with the MathML output.
+    shadowRoot.innerHTML = TeXZilla.toMathMLString(
+      tex,
+      display === "block",
+      dir === "rtl",
+      true
+    );
+  } catch (e) {
+    // Parsing failed: use an <merror> with the original LaTeX input.
+    shadowRoot.innerHTML =
+      "<math><merror><mtext>" + tex + "</mtext></merror></math>";
+  }
+}
+
+class LatexElement extends HTMLElement {
+    constructor() {
+      super()
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.adoptedStyleSheets = [SHEET];
+      this.mo = new MutationObserver((_recs) => {
+        updateMathMLOutput(this);
+      })
+      this.mo.observe(this, { characterData: true, childList: true, attributes: true });
+      updateMathMLOutput(this);
     }
-
-    class LaTeX_Element extends HTMLElement {
-        constructor() {
-          super()
-          this.attachShadow({ mode: "open" });
-          this.mo = new MutationObserver((recs) => {
-            updateMathMLOutput(this);
-          })
-          this.mo.observe(this, { characterData: true, childList: true, attributes: true });
+    
+    /**
+   * @param {string} aName
+   * @param {string | null} _aOld
+   * @param {string | null} aNew
+   */
+    attributeChangedCallback(aName, _aOld, aNew) {
+      if (aName === "dir" || aName === "display") {
+        const shadowRoot = /** @type {ShadowRoot} */(this.shadowRoot);
+        if (aNew === null) {
+          shadowRoot.firstElementChild?.removeAttribute(aName);
+        } else {
+          shadowRoot.firstElementChild?.setAttribute(aName, aNew);
         }
+      }
+    }
+} 
 
-        attributeChangedCallback(aName, aOld, aNew) {
-          if (aName === "dir" || aName === "display") {
-            if (aNew === null) {
-              this.shadowRoot.firstElementChild.removeAttribute(aName);
-            } else {
-              this.shadowRoot.firstElementChild.setAttribute(aName, aNew);
-            }
-          }
-        }
-    } 
-
-    customElements.define("la-tex", LaTeX_Element);
-  })();
+customElements.define("la-tex", LatexElement);
